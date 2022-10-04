af2ab9f4a1e7214b5f4cbfd75ad50975b07441ea
Igor_U <fixtor75@gmail.com>
2022-10-04 13:28:04 +0300

    Задание 2 Готово

---
+++ Exe2.py
@@ -0,0 +1,18 @@
+# Задайте натуральное число N. Напишите программу, 
+# которая составит список простых множителей числа N.
+
+number = int(input("Введите число:"))
+new_list = []     
+div = 2
+while(div <= number):
+    if (number%div ==0):
+        new_list.append(div)
+        number = number/div
+    else:
+        div += 1 
+print (new_list)
+        
+
+        
+
+

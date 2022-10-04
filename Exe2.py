61ce533994f419281d2fbdb212136f0984f2b404
Igor_U <fixtor75@gmail.com>
2022-10-04 13:45:56 +0300

    Exercise2 is ready. upd

--- Exe2.py
+++ Exe2.py
@@ -5,7 +5,7 @@ number = int(input("Введите число:"))
 new_list = []     
 div = 2
 while(div <= number):
-    if (number%div ==0):
+    if (number % div ==0):
         new_list.append(div)
         number = number/div
     else:

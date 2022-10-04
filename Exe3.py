24813f048b3f4fe9a9060021a08f03159c05f73f
Igor_U <fixtor75@gmail.com>
2022-10-04 15:28:58 +0300

    Exercise 3 is ready.. upd

--- Exe3.py
+++ Exe3.py
@@ -5,9 +5,7 @@ from uCreate_user_list import Int_list_create
 new_list = Int_list_create()
 new_list1 = []
 
-for x in new_list:
-    if new_list.count(x) == 1:
-         new_list1.append(x)
+a = [new_list1.append(x) for x in new_list if new_list.count(x) == 1]
 
 print(new_list)
 print(new_list1)
